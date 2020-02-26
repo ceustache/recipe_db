a8b5ecd952fa3525e0e56c17bd7c54b3b7cb1266
cris <criseustache44@gmail.com>
2020-02-25 20:36:17 -0600

    Troubleshooting heroku changed config file

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ module RecipeDb
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+    config.assets.initialize_on_precompile = false
+    config.secret_key = '-- secret key --'
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
